cba222d0b0eaf7e609192f56a875c27b51d783df
Alim Charaniya <alimcharaniya25@gmail.com>
2017-04-18 20:36:52 -0700

    Added rpi-gpio library and test code
    Turns LED on port 12 on and off according to state

--- client/rasapp.js
+++ client/rasapp.js
@@ -1,10 +1,25 @@
-var socket = require("socket.io-client")("http://localhost:3000");
+var socket = require("socket.io-client")("http://10.113.31.109:8080");
+var gpio = require("rpi-gpio");
+
+//start up code
+process.on("SIGINT", function(){
+  gpio.write(12, true, function(){
+    gpio.destroy(function(){
+      process.exit();
+    });
+  });
+});
+
+gpio.setup(12, gpio.DIR_OUT, function(){
+  gpio.write(12, false); //turns led off
+});
+
 
 socket.on("connect", function(){
   console.log("connected to server");
 
   socket.on("stateChanged", function(state){
     console.log("new state is: " + state);
-
+    gpio.write(12, state); //writes to pin 12 the state of the checkbox
   });
 });
