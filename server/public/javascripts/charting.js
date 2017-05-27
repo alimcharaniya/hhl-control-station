c40e7a5100c242dd8250218f3fad4c7a2a2e2517
David Judilla <davidfjudilla@gmail.com>
2017-05-27 14:24:32 -0700

    Progress made on google charts. New fake data is added every second

--- server/public/javascripts/charting.js
+++ server/public/javascripts/charting.js
@@ -1,31 +1,36 @@
+/* 
+ * The function drawGraph gets called once all the html and css are done processing to the page.
+ * drawGraph() instantiates and draws the google chart then sets an interval, where every 1 second
+ * new data is inserted and updates the chart (See updateChart and the setInterval that calls it)
+ */
 google.charts.load('current', {packages: ['corechart', 'line']});
-google.charts.setOnLoadCallback(drawBasic);
+google.charts.setOnLoadCallback(drawGraph);
 
-function drawBasic() {
-      var data = new google.visualization.DataTable();
-      data.addColumn('number', 'X');
-      data.addColumn('number', 'Dogs');
-    var options = {
-      hAxis: {
-        title: 'Time'
-      },
-      vAxis: {
-        title: 'Popularity'
-      }
-    };
-    var count;
-    var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+function drawGraph() {
+  var data = new google.visualization.DataTable();
+  data.addColumn('number', 'X');
+  data.addColumn('number', 'Dogs');
+
+  var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+  var options = {
+    hAxis: {
+      title: 'Time'
+    },
+    vAxis: {
+      title: 'Popularity'
+    }
+  };
+  chart.draw(data, options);
+
+  setInterval(updateChart, 1000);
+  var count = 0;
+
+  function updateChart(){
+    console.log(count);
+    var x = [count, count+2];
+    data.insertRows(count, [x]);
     chart.draw(data, options);
 
-    for(count = 0; count < 10; count++){
-      var myVar = setInterval(dosomething, 1000);
-
-      var x = [count, count+2];
-      data.insertRows(count, [x]);
-      chart.draw(data, options);
-      function dosomething(){
-        console.log(count);
-
-      }
-    }
+    count++
+  }
 }
